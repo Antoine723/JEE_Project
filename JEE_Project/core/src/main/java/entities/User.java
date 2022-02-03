439df1528b3e0acf5c3a576103c0f66f2031eac0
Antoine <antoinea723@gmail.com>
2022-02-03 14:14:55 +0100

    Fill User entity

--- JEE_Project/core/src/main/java/entities/User.java
+++ JEE_Project/core/src/main/java/entities/User.java
@@ -1,4 +1,43 @@
 package entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
 public class User {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gender")
+    private int gender;
+
+    @Column(name = "birthdate")
+    private Date birthdate;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postcode")
+    private Long postcode;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "nbrComments")
+    private int nbrComments;
+
 }
