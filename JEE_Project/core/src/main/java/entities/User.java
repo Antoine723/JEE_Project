b712bf54ab0568536e055dd63ee7b35eaa643fe0
Antoine <antoinea723@gmail.com>
2022-02-03 14:19:55 +0100

    Add generated value for User id

--- JEE_Project/core/src/main/java/entities/User.java
+++ JEE_Project/core/src/main/java/entities/User.java
@@ -3,9 +3,7 @@ package entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -13,6 +11,7 @@ import java.util.Date;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
