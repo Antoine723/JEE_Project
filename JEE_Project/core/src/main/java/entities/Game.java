51a59d440b1e6845e2ba2959162b57b93bc895bb
Antoine <antoinea723@gmail.com>
2022-02-03 15:30:01 +0100

    Link games with consoles

--- JEE_Project/core/src/main/java/entities/Game.java
+++ JEE_Project/core/src/main/java/entities/Game.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -15,4 +19,8 @@ public class Game extends Product{
 
     @Column(name="offline_players_number")
     private int onlinePlayersNumber;
+
+    @OneToMany
+    @JoinColumn(name = "game_id")
+    private List<Console> consoles = new ArrayList<>();
 }
