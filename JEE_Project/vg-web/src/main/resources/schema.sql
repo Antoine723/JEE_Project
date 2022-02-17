cb32e558cf8881151450f923753a5650265a7441
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-02-17 10:11:05 +0100

    sql : drop tables before build

--- JEE_Project/vg-web/src/main/resources/schema.sql
+++ JEE_Project/vg-web/src/main/resources/schema.sql
@@ -1,12 +1,25 @@
+SET FOREIGN_KEY_CHECKS = 0;
+drop table comment;
+drop table console;
+drop table game;
+drop table games_on_consoles;
+drop table hibernate_sequence;
+drop table orders;
+drop table orders_products;
+drop table user;
+SET FOREIGN_KEY_CHECKS = 1;
+
 create table comment (id bigint not null, content varchar(255), rating integer, product_id bigint, user_id bigint, primary key (id));
 create table console (id bigint not null, img varchar(255), name varchar(255), price float, quantity integer, rating float, release_year date, primary key (id));
 create table game (id bigint not null, img varchar(255), name varchar(255), price float, quantity integer, rating float, release_year date, offline_players_number integer, online_players_number integer, primary key (id));
 create table games_on_consoles (game_id bigint not null, console_id bigint not null);
 create table hibernate_sequence (next_val bigint);
-insert into hibernate_sequence values ( 1 );
 create table orders (id bigint not null, address varchar(255), order_number binary(255), user_id bigint, primary key (id));
 create table orders_products (order_id bigint not null, product_id bigint not null);
 create table user (id bigint not null, address varchar(255), mail varchar(255), name varchar(255), nbr_comments integer, password varchar(255), primary key (id));
+
+insert into hibernate_sequence values ( 1 );
+
 alter table comment add constraint FK8kcum44fvpupyw6f5baccx25c foreign key (user_id) references user (id);
 alter table games_on_consoles add constraint FKfkdp40taw91m0hd9nrrcy8vyr foreign key (console_id) references console (id);
 alter table games_on_consoles add constraint FKc9rccrnuycxbrjqbg5ydhoytp foreign key (game_id) references game (id);
