6fb7590874cadb58831babe3696478f5cfb4bc48
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-02-21 20:50:50 +0100

    home search : jsonisation ok

--- JEE_Project/vg-web/src/main/java/com/videoGamesWeb/vgweb/controllers/ProductsController.java
+++ JEE_Project/vg-web/src/main/java/com/videoGamesWeb/vgweb/controllers/ProductsController.java
@@ -1,12 +1,15 @@
 package com.videoGamesWeb.vgweb.controllers;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.videoGamesWeb.vgcore.dto.SearchDTO;
+import com.videoGamesWeb.vgcore.entity.Console;
+import com.videoGamesWeb.vgcore.entity.Game;
 import com.videoGamesWeb.vgcore.entity.Product;
 import com.videoGamesWeb.vgcore.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -28,10 +32,29 @@ public class ProductsController {
     }
 
     @PostMapping(value = "/search")
-    public ResponseEntity<String> addComment(@RequestBody SearchDTO searchDTO){
+    public ResponseEntity<String> addComment(@RequestBody SearchDTO searchDTO) throws JsonProcessingException {
         logger.info("recieve {}", searchDTO.getInput());
-        List<Product> results = productService.searchWithText(searchDTO.getInput());
+        List<Product> results = productService.searchWithText(searchDTO.getInput())
+                .stream()
+                .peek(product -> {
+                    product.setComments(null);
+                    product.setOrders(null);
+                    //set images ?
+                    switch (product.getClass().getSimpleName()) {
+                        case "Game":
+                            ((Game) product).setConsoles(null);
+                            break;
+                        case "Console":
+                            ((Console) product).setGames(null);
+                            break;
+                    }
+                })
+                //.sorted(Comparator.comparing(Product::getName).reversed())
+                .collect(Collectors.toList());
         logger.info(String.valueOf(results.size()));
-        return ResponseEntity.ok(results.toString());
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(results);
+        logger.info("result : {}", jsonString);
+        return ResponseEntity.ok(jsonString);
     }
 }
