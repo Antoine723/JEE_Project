73329e2e1721df4aef34d9ff148e3ca19862d021
Antoine <antoinea723@gmail.com>
2022-02-22 19:12:55 +0100

    WIP add payment page

---
+++ JEE_project/vg-web/src/main/java/com/videoGamesWeb/vgweb/controllers/PaymentViewController.java
@@ -0,0 +1,30 @@
+package com.videoGamesWeb.vgweb.controllers;
+
+import com.videoGamesWeb.vgcore.entity.User;
+import com.videoGamesWeb.vgcore.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class PaymentViewController {
+
+    private final static Logger logger = LoggerFactory.getLogger(PaymentViewController.class);
+    private final UserService userService;
+
+    public PaymentViewController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("payment")
+    public String payment(Model model, HttpSession session){
+        User user = this.userService.getById((long) session.getAttribute("userID"));
+        logger.info("User : {}", user);
+        model.addAttribute("user", user);
+        return "payment";
+    }
+}
---
+++ JEE_project/vg-web/src/main/webapp/WEB-INF/jsp/payment.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Validation de la commande</title>
+    <jsp:include page="head.jsp"/>
+    <link href="<c:url value="/css/payment.css"/>" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <p>
+    <h1>Informations de livraison :</h1>
+    <form>
+        <label id="labelName">Nom</label>
+    </form>
+    </p>
+
+</body>
+</html>
